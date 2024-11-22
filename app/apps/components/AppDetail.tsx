a1ccd5fe79b067df5be9e2c6d7942067faa373d4
System Administrator <sciemesfin55@gmail.com>
2024-11-22 15:12:40 +0300

    modal bg ormal

--- app/apps/components/AppDetail.tsx
+++ app/apps/components/AppDetail.tsx
@@ -14,7 +14,7 @@ import show from '@/lib/toast'
 interface PROPS {
     open: boolean,
     app: APP_INFO | undefined,
-    toggleModal: (buttonName: string) => void,
+    toggleModal: (buttonName:string) => void,
     siteName: string
 }
 
@@ -35,10 +35,10 @@ export default function AppDetail({
             "siteName": siteName,
             "app": app?.appName
         }
-        api.create(payload, installed ? "/uninstall-app" : "/install-app")
+        api.create(payload, installed?"/uninstall-app": "/install-app")
             .then(() => {
                 show.success(`${app?.title} has been ${installed ? 'uninstalled' : "installed"} successfully.`)
-            })
+                 })
             .catch(() => {
                 show.error(`Unable to install ${app?.title} to site ${siteName} right now. Try again.`)
             })
@@ -47,7 +47,7 @@ export default function AppDetail({
                 toggleModal("installUninstall")
             });
     }
-
+  
 
     return (
         <Dialog open={open} onClose={() => { }} className="relative z-10">
@@ -60,7 +60,7 @@ export default function AppDetail({
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <DialogPanel
                         transition
-                        className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                        className="border border-gray-200 bg-gray-50 relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
                     >
                         <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                             <div className="sm:flex sm:items-start">
@@ -99,7 +99,7 @@ export default function AppDetail({
                                     <span className='text-gray-600'>{installed ? 'Uninstalling' : "Installing"} {app?.title} ... </span>
                                 </div>
                                 : <div className='sm:flex sm:flex-row-reverse'>
-                                    {app?.appName != "erpnext" && <button
+                                   {app?.appName!="erpnext"&& <button
                                         onClick={installOrInstallApp}
                                         className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
                                     >
